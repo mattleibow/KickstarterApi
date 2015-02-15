168d370f5720e628223a7c85fa443443b7a4198f
Matthew Leibowitz <mattleibow@live.com>
2015-02-15 19:53:45 +0200

    Added the initial test project

---
+++ KickstarterApi.Tests/FirstTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Kickstarter.Api;
+using Kickstarter.Api.Queries;
+
+namespace Kickstarter.KickstarterApi.Tests
+{
+    [TestClass]
+    public class FirstTest
+    {
+        [TestMethod]
+        public async Task FirstTestMethod()
+        {
+            var client = new KickstarterClient();
+
+            var session = await client.StartSession("EMAIL", "PASSWORD");
+
+            var tableTop = await session.Query(new FindCategory("Tabletop Games"));
+
+            var projects = new DiscoverProjects()
+                          .InCategory(tableTop)
+                          .InStatus("live")
+                          .SortedBy("launch_date")
+                          .Take(200);
+
+            var query = from p in await session.Query(projects) 
+                        orderby p.LaunchedAt 
+                        select p;
+
+            var list = query.ToList();
+        }
+    }
+}
